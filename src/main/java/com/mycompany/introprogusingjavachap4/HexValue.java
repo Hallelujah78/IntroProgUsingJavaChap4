c3539263ad795a19b8feff87b42c817bd48a5c34
Gavan Browne <gavan.browne@gmail.com>
2025-02-22 21:39:44 +0000

    Finish exercise 2, chapter 4.
    Added hexStringToInt method. Takes a hex string as param. Returns the integer value of the hex string if valid, or returns -1 if the hex string contains non-hexadecimal characters.

--- src/main/java/com/mycompany/introprogusingjavachap4/HexValue.java
+++ src/main/java/com/mycompany/introprogusingjavachap4/HexValue.java
@@ -12,6 +12,42 @@ import java.util.HexFormat;
  */
 public class HexValue {
 
+    // hex value 'FADE'
+    // F = 15
+    // A = 10
+    // D = 13
+    // E = 14
+    // value of FADE (base 16) is:
+    // 0*16+15
+    // 15*16+10 = 250
+    // 250*16 + 13 = 4013 - is (15*16+10) x 16 + 13 is 15 x 16^2 + 10 x 16^1 + 13
+    // 4013*16 + 14 = 64222 base 10 - is (15 x 16^2 + 10 x 16^1 + 13) x 16 + 14 - is 15x16^3 + 10 x 16^2 + 13 x 16^1 + 14 x 16^0
+    /* Standard way:
+    15x16^3 + 10x16^2 + 13 x 16^1 + 14x16^0
+     */
+    public static int hexStringToInt(String value) {
+
+        // Store the cumulative value.
+        int cumValue = 0;
+
+        for (int i = 0; i < value.length(); i++) {
+
+            // Value of char at i.
+            int charVal = hexValue(value.charAt(i));
+
+            // If hexValue returns -1 then value is not a valid hex string.
+            if (charVal == -1) {
+
+                return -1;
+            }
+
+            cumValue = cumValue * 16 + charVal;
+
+        }
+        return cumValue;
+
+    }
+
     public static int hexValue(char character) {
 
         // Normalize input - we consider a-f to have same hex values as A-F.
--- src/main/java/com/mycompany/introprogusingjavachap4/IntroProgUsingJavaChap4.java
+++ src/main/java/com/mycompany/introprogusingjavachap4/IntroProgUsingJavaChap4.java
@@ -25,5 +25,20 @@ public class IntroProgUsingJavaChap4 {
         // Exercise 2 - get the hex value from a char
         int intValOfHexValue = HexValue.hexValue('G');
         System.out.println("\nThe int val: " + intValOfHexValue);
+
+        // Exercise 2 part deux: calculate the integer value of a hex string.
+        // Get input from user.
+        System.out.println("Enter a hex string:\t");
+        String hexString = scan.nextLine();
+
+        int intVal = HexValue.hexStringToInt(hexString);
+        if (intVal == -1) {
+            // Print error.
+            System.out.println(hexString + " is not a valid hex string.");
+        } else {
+            // It is a valid hex string, print the integer value.
+            System.out.println("The integer value of " + hexString + " is " + intVal);
+        }
+
     }
 }
