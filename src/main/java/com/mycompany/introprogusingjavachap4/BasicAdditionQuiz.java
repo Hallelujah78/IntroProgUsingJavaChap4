c37b70484bc03a435d9208d13394d85c2de5a6f8
Gavan Browne <gavan.browne@gmail.com>
2025-02-27 20:47:03 +0000

    Finish exercise 7 chapter 4
    Added gradeQuiz method. Refactored createQuiz so it now takes a parameter that determines how many questions are in the quiz.

--- src/main/java/com/mycompany/introprogusingjavachap4/BasicAdditionQuiz.java
+++ src/main/java/com/mycompany/introprogusingjavachap4/BasicAdditionQuiz.java
@@ -12,9 +12,10 @@ import java.util.Scanner;
  */
 public class BasicAdditionQuiz {
 
-    private static final int[] num1 = new int[10];
-    private static final int[] num2 = new int[10];
-    private static final int[] userAnswer = new int[10];
+    private static final int MAX_SCORE = 100;
+    private static int[] num1;
+    private static int[] num2;
+    private static int[] userAnswer;
 
     // 10 questions
     // each question is a simple addition: 17+42
@@ -30,7 +31,12 @@ public class BasicAdditionQuiz {
     /**
      * Initialize the arrays with random numbers
      */
-    static void createQuiz() {
+    static void createQuiz(int numQuestions) {
+
+        num1 = new int[numQuestions];
+        num2 = new int[numQuestions];
+        userAnswer = new int[numQuestions];
+
         for (int i = 0; i < num1.length; i++) {
             // Add a random integer to num1
             num1[i] = (int) (Math.random() * 100);
@@ -53,17 +59,45 @@ public class BasicAdditionQuiz {
         System.out.println("Welcome to the Simple Addition Quiz!");
         System.out.println("Please enter your answer for each question.");
         for (int i = 0; i < num1.length; i++) {
-            System.out.println(num1[i] + " + " + num2[i]);
+            System.out.print(num1[i] + " + " + num2[i] + ": ");
             String userInput = scan.nextLine();
             userAnswer[i] = Integer.parseInt(userInput); // while loop here to handle exception
         } // end of for loop.
 
     }
 
+    /**
+     * Grade the quiz and provide feedback to user.
+     */
+    static void gradeQuiz() {
+
+        // Store the user score.
+        double userScore = 0.0;
+
+        for (int i = 0; i < num1.length; i++) {
+
+            int correctAnswer = num1[i] + num2[i];
+
+            String answerStatus = correctAnswer == userAnswer[i] ? "Correct" : "Incorrect";
+
+            System.out.println("Question: " + num1[i] + " + " + num2[i]);
+            System.out.println("Your answer: " + userAnswer[i] + ". " + answerStatus);
+            if (answerStatus.equals("Incorrect")) {
+                System.out.println("Correct Answer: " + correctAnswer);
+                continue;
+            }
+            // Answer was correct, increment user score.
+            userScore += MAX_SCORE / num1.length;
+        }
+        // Output grade.
+        System.out.println("Your score: " + userScore + " / " + MAX_SCORE);
+    }
+
     public static void main(String[] args) {
 
-        createQuiz();
+        createQuiz(10);
         administerQuiz();
+        gradeQuiz();
     }
 
 }
